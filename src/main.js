bea61c94766fa6ead5ed914fd7d826023b7b5d01
Vinicius Moret <viniciusrcr@hotmail.com>
2017-12-15 16:38:21 -0500

    test

--- src/main.js
+++ src/main.js
@@ -7,3 +7,24 @@ function sum() {
 }
 
 sum();
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  hello() {
+    console.log(`I'm ${this.name}`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, sound) {
+    super(name);
+    this.sound = sound;
+  }
+
+  bark() {
+    console.log(`${this.sound} I'm ${this.name}`);
+  }
+}
